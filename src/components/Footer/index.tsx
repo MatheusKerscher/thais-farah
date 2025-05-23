94765cacc9a601d53794fdce15883e537fba45a1
MatheusKerscher <kerschermatheus12@gmail.com>
2025-05-23 07:58:27 -0300

    Update text color of footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <footer className="px-6 md:px-15 pb-5 pt-6 bg-white flex flex-col justify-center items-center text-sm">
+    <footer className="px-6 md:px-15 pb-5 pt-6 bg-white text-gray-400 flex flex-col justify-center items-center text-sm">
       <span className="font-bold">Dra Thaís Farah</span>
       <span className="text-xs">CRM 45740 | RQE 34216</span>
 
@@ -12,7 +12,7 @@ const Footer = () => {
       <span className="mt-3 mb-1">&copy; {year} - Todos os Direitos Reservados</span>
 
       <span className="mt-5">
-        Desenvolvido por{" "}
+        Desenvolvido com ❤️ por{" "}
         <a
           href="https://matheuskerscher.github.io/links/"
           target="_blank"
